cca7af7449ac9defa69d8bd05625b051b0a1398d
a3hen <841361233@qq.com>
2024-03-04 16:43:20 +0800

    修正日志名

--- utils/utils.py
+++ utils/utils.py
@@ -76,7 +76,7 @@ class Log(object):
     @staticmethod
     def set_handler(logger):
         now_time = datetime.datetime.now().strftime('%Y-%m-%d')
-        file_name = f'vsdsadm_{now_time}.log'
+        file_name = f'vsdsencryptor_{now_time}.log'
         fh = logging.FileHandler(file_name, mode='a', encoding='utf-8')
         fh.setLevel(logging.DEBUG)
         formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s", datefmt='%Y-%m-%d %H:%M:%S')
